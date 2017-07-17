71ccc2ceab587d42116c9162f4fdf014949575b6
Byeongjo kim <qocnajfl3@naver.com>
2017-07-17 04:46:45 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'index/b'
 
   get 'index/c'
+  
+  get 'index/phpinfo.php'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
